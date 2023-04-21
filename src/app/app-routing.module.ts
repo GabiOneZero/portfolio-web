dbf6358e63be3dc411c53be4ac19147524657525
Gabriel Sánchez <gabionezero@gmail.com>
2023-04-21 14:45:14 +0200

    Add routing to Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./home/home.module')
+      .then((moduleRoute) => moduleRoute.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
